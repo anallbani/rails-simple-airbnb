f27708cdd6f7367883a40b9f913145c8310f16fb
Anxhoel Nallbani <anxhoelnallbani@gmail.com>
2025-05-28 14:36:48 +0200

    Add FlatsController and full CRUD actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'Stylish house with balcony and garden, near the river Thames.',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'Unique flat inside the iconic St Pancras clock tower.',
+  price_per_night: 110,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Modern Apartment in Soho',
+  address: '23 Wardour St London W1D 6PN',
+  description: 'Trendy apartment in the heart of Soho with views over Chinatown.',
+  price_per_night: 90,
+  number_of_guests: 4
+)
